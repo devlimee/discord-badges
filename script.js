3f99155f904d39ffb90ad31a190ada1f3e1b18d1
limee <41487884+devlimee@users.noreply.github.com>
2019-10-27 12:42:33 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const badge = webpackJsonp.push([[], { __extra_id__: (module, exports, badge) => (module.exports = badge) }, [['__extra_id__']]]);
+for (let setBadge in badge.c) {
+  if (badge.c.hasOwnProperty(setBadge)) {
+    let user = badge.c[setBadge].exports;
+    if (user && user.__esModule && user.default && user.default.getCurrentUser) {
+      user.default.getCurrentUser().flags |= 1;
+      user.default.getCurrentUser().flags |= 2;
+      user.default.getCurrentUser().flags |= 3;
+      user.default.getCurrentUser().flags |= 4;
+      user.default.getCurrentUser().flags |= 5;
+      user.default.getCurrentUser().flags |= 6;
+      user.default.getCurrentUser().flags |= 7;
+      user.default.getCurrentUser().flags |= 8;
+      user.default.getCurrentUser().flags |= 9;
+      user.default.getCurrentUser().flags |= 10;
+    break;
+   }
+  }
+}
