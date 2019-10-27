8a70eb2163ad64b200ae8f6fee10fa5c1c01cb0b
limee <41487884+devlimee@users.noreply.github.com>
2019-10-27 12:51:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,8 @@ for (let setBadge in badge.c) {
     if (user && user.__esModule && user.default && user.default.getCurrentUser) {
       user.default.getCurrentUser().flags |= 1;
       user.default.getCurrentUser().flags |= 2;
-      user.default.getCurrentUser().flags |= 3;
       user.default.getCurrentUser().flags |= 4;
-      user.default.getCurrentUser().flags |= 5;
-      user.default.getCurrentUser().flags |= 6;
-      user.default.getCurrentUser().flags |= 7;
       user.default.getCurrentUser().flags |= 8;
-      user.default.getCurrentUser().flags |= 9;
-      user.default.getCurrentUser().flags |= 10;
     break;
    }
   }
